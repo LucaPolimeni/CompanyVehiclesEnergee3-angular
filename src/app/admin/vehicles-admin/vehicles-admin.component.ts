3486aeda2d42d553254340c4561094fbcabfbab3
LucaPolimeni <luca.polimeni@outlook.it>
2022-07-19 14:20:25 +0200

    Add Model, Manufacturer. Vehicle

--- src/app/admin/vehicles-admin/vehicles-admin.component.ts
+++ src/app/admin/vehicles-admin/vehicles-admin.component.ts
@@ -22,6 +22,11 @@ export class VehiclesAdminComponent implements OnInit, OnDestroy {
   add = false;
   addedVehicle = false;
   showButton = true;
+  calledGetManufacturer = false;
+  addManufacturerButton = false;
+  manufacturerAdded = false;
+  addModelButton = false;
+  modelAdded = false;
   targa = "";
   produttore = "";
   modello = "";
@@ -37,6 +42,10 @@ export class VehiclesAdminComponent implements OnInit, OnDestroy {
     'Attiva'
   ];
 
+  modelsArray = [];
+  manufacturersArray = [];
+  modelsByManufacturerArray = [];
+
   vehiclesArray: VehicleModel[] = [];
   dataSource: MatTableDataSource<any>;
 
@@ -44,6 +53,7 @@ export class VehiclesAdminComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.getVehicles();
+    this.getModels();
   }
 
   ngOnDestroy(): void {
@@ -53,20 +63,40 @@ export class VehiclesAdminComponent implements OnInit, OnDestroy {
   addVehicle(){
     this.add = !this.add;
     this.showButton = !this.add;
+    if (!this.calledGetManufacturer)
+      this.getManufacturer();
+    this.calledGetManufacturer = true;
   }
 
   onSubmit(form: NgForm){
     console.log(form.value);
   }
 
-  onAdd(form: NgForm){
+  onAddVehicle(form: NgForm){
     this.addedVehicle = true;
     console.log("added", form.value);
     this.targa = (form.value.licensePlate).toUpperCase();
-    this.produttore = form.value.manufacturer;
+    this.produttore = form.value.manufacturer
     this.modello = form.value.model;
     this.alimentazione = form.value.fuel;
-    this.anno = form.value.year;
+
+    console.log("targa -> ", this.targa)
+    console.log("id modello -> ", this.modello)
+    console.log("alimentazione -> ", this.alimentazione)
+
+    this.http.post("http://localhost:8080/api/vehicles/newVehicle", 
+      {
+        "id": this.targa,
+        "fuel": this.alimentazione,
+        "modelId": {
+            "id": this.modello
+        },
+        "active": true
+      }
+      ).subscribe(responseData => {
+        console.log(responseData);
+        this.modelAdded = true;
+      });
   }
 
   getVehicles(){
@@ -97,10 +127,89 @@ export class VehiclesAdminComponent implements OnInit, OnDestroy {
       });
   }
 
-  insertVehicles(){
-    //this.http.post("http://localhost:8080/api/vehicles/").subscribe();
+  getModels(){
+    this.http.get("http://localhost:8080/api/vehicles/models")
+    .subscribe(responseData => {
+      for (const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          this.modelsArray.push({...responseData[key], id:key});
+        }
+      }
+      console.log(this.modelsArray);
+    })
   }
 
+  getManufacturer(){
+    this.http.get("http://localhost:8080/api/vehicles/manufacturer")
+    .subscribe(responseData => {
+      for (const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          this.manufacturersArray.push({...responseData[key], myId:key});
+        }
+      }
+      console.log(this.manufacturersArray);
+    })
+  }
 
+  getModelsByManufacturer(event){
+    if (event.target.value == "add"){
+      this.addManufacturerButton = true;
+    } else{
+      this.addManufacturerButton = false;
+      this.modelsByManufacturerArray = [];
+      const manufacturerName = event.target.value;
+      console.log(manufacturerName);
+    
+      this.http.get("http://localhost:8080/api/vehicles/modelByManufacturer/" + manufacturerName)
+      .subscribe(responseData => {
+        for (const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            this.modelsByManufacturerArray.push({...responseData[key], myId:key});
+          }
+        }
+        console.log(this.modelsByManufacturerArray);
+      })
+    }
+  }
+
+  addManufacturer(form: NgForm){
+    const name = form.value.manufacturerName
+    this.http.post("http://localhost:8080/api/vehicles/newManufacturer", {"name": name})
+    .subscribe(responseData => {
+      console.log(responseData);
+      this.manufacturerAdded = true;
+    });
+  }
+
+  addModelEvent(event){
+    if(event.target.value == "add"){
+      this.addModelButton = true;
+    } else{
+      this.addModelButton = false;
+    }
+  }
+
+  addModel(form: NgForm){
+    const manufacturerId = form.value.manufacturer;
+    const modelName = form.value.modelName
+    const year = form.value.year
+  
+    console.log("id manufacturer -> ", manufacturerId)
+    console.log("name model -> ", modelName)
+    console.log("year -> ", year)
+
+       this.http.post("http://localhost:8080/api/vehicles/newModel", 
+       {
+        "name": modelName,
+        "yearProd": year,
+        "manufacturerId": {
+            "id": manufacturerId
+         }
+       }
+       ).subscribe(responseData => {
+        console.log(responseData);
+        this.modelAdded = true;
+      });
+  }
 
 }
